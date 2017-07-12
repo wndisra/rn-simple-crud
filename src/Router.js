c55c3530e428a56838eb9cfc31749902b8b404cc
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-07-12 21:43:47 +0700

    Add EditForm & ShowPostDetail components

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import PostList from './components/PostList';
 import CreateForm from './components/CreateForm';
+import EditForm from './components/EditForm';
+import showPostDetail from './components/ShowPostDetail';
 
 const RouterComponent = () => {
   return (
@@ -16,6 +18,18 @@ const RouterComponent = () => {
         key="createForm"
         component={CreateForm}
         title={'Create Post'}
+      />
+
+      <Scene
+        key="editForm"
+        component={EditForm}
+        title={'Edit Post'}
+      />
+
+      <Scene
+        key="showPostDetail"
+        component={showPostDetail}
+        title={'Detail Post'}
       />
     </Router>
   );
